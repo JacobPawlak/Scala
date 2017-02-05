41da7ebd605455571b5cb42d2ad51a15437d932a
Jacob Cass Pawlak <jacob.pawlak@uky.edu>
2017-02-05 12:24:54 -0500

    Add files via upload

---
+++ cToFExample.scala
@@ -0,0 +1,12 @@
+/** Jacob Pawlak
+*/
+val a: Double = 27.2
+val b: Double = a * 9 / 5
+val c: Double = b + 32
+println(a + " In celsius is equivalent to " + c + " in fahrenheit.")
+
+/** Cast to an int
+*/
+
+val d: Int = c.toInt
+println(a + " In celsius is equivalent to " + d + " in fahrenheit (rounded down). ")
